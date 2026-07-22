26d7af27534b6c573bab57abc8d79b7b283248fd
karem6basha-svg <karem6.basha@gmail.com>
2026-07-22 16:03:14 +0800

    Update ticket.py

--- Week_9/ticket.py
+++ Week_9/ticket.py
@@ -1,1 +1,23 @@
 
+def create_ticket():
+    print("=== IT Helpdesk Ticket ===")
+
+    name = input("Student Name : ")
+    student_id = input("Student ID : ")
+    issue = input("Issue : ")
+    location = input("Location : ")
+    priority = input("Priority (High/Medium/Low): ")
+
+    # Assign technician based on priority level
+    if priority.lower() == "high":
+        technician = "Ahmad"
+    elif priority.lower() == "medium":
+        technician = "Siti"
+    elif priority.lower() == "low":
+        technician = "Ali"
+    else:
+        technician = "Unassigned"
+
+    status = "Pending"
+
+    return (name, student_id, issue, location, priority, technician, status)
