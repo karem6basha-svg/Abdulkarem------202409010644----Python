b43d5fbea9909849b0eab61dabd132bbd12897bc
karem6basha-svg <karem6.basha@gmail.com>
2026-07-22 16:02:41 +0800

    Update display.py

--- Week_9/display.py
+++ Week_9/display.py
@@ -1,1 +1,12 @@
+def display_ticket(name, student_id, issue, location, priority, technician, status):
+    print()
+    print("========== HELPDESK TICKET ==========")
+    print(f"Student Name : {name}")
+    print(f"Student ID   : {student_id}")
+    print(f"Issue        : {issue}")
+    print(f"Location     : {location}")
+    print(f"Priority     : {priority}")
+    print(f"Technician   : {technician}")
+    print(f"Status       : {status}")
+    print("=======================================")
 
