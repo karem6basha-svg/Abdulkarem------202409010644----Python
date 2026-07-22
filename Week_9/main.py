0d3fcdd78f41dd46b959daab13b13f4eac0bc047
karem6basha-svg <karem6.basha@gmail.com>
2026-07-22 14:07:34 +0800

    Update main.py

--- Week_9/main.py
+++ Week_9/main.py
@@ -1,1 +1,16 @@
+from ticket import create_ticket
+from display import display_ticket
 
+
+def main():
+    name, student_id, issue, location, priority, technician, status = create_ticket()
+
+    if technician == "Unassigned":
+        print("\nInvalid priority entered. Please use High, Medium, or Low.")
+        return
+
+    display_ticket(name, student_id, issue, location, priority, technician, status)
+
+
+if __name__ == "__main__":
+    main()
